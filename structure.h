96966c480a8e5fc2f056c8ea9d72f46bba051d09
Achecas <quentin.turcotti@hotmail.fr>
2022-11-23 17:09:02 +0100

    creation de structure

--- structure.h
+++ structure.h
@@ -1,1 +1,43 @@
 #include <stdio.h>
+
+enum categorie_avion
+{
+    avions_de_ligne,
+    avions_daffaires,
+    avions_legers
+}
+
+enum categorie_piste
+{
+    moyenne,
+    grande,
+    petite
+}
+
+typedef struct avion
+{
+    int identifiant;
+    int categorie_avion;
+    int etat;
+    int nb_passager;
+}
+
+typedef struct attente attente;
+struct attente
+{
+    avion avion;
+    avion *suivant;
+}
+
+typedef struct piste
+{
+    int nb_piste;
+    int longueur;
+    int categorie_piste
+    int nb_att_max;
+    attente* attente
+}
+
+
+
+
